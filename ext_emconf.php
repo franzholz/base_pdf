a528ab60002e729609026d1096075ca62ac403a5
Franz Holzinger <holzinger@ttproducts.de>
2020-09-20 00:12:10 +0200

    * upgrade to Dompdf 0.8.6: See the file Vendor/version.txt and https://github.com/dompdf/dompdf/releases .

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'DOMPDF Library',
     'description' => 'This provides the DOMPDF library.',
     'category' => 'misc',
-    'version' => '0.0.1',
+    'version' => '0.1.0',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -18,8 +18,8 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => 'jambage.com',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.4.0-7.3.99',
-            'typo3' => '6.2.0-9.5.99'
+            'php' => '7.1.0-7.3.99',
+            'typo3' => '8.7.0-9.5.99'
         ),
         'conflicts' => array(
         ),
